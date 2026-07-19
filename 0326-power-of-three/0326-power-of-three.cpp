52af144ed79c3d8314f800b670b64ae4b8808098
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-19 22:24:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (75.27%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-         if(n<=0)
-      {
-        return false;
-      }
+    bool isPowerOfThree(int n) 
+    {
+        if(n<=0)
+          return false;
         while(n%3==0)
         {
-            n=n/3;
-        }
-            if(n==1)
-            {
-                return true;
-            }
-             else
-            {
-                return false;
-            }
+            n/=3;
+        }  
+        return n==1;
     }
 };
