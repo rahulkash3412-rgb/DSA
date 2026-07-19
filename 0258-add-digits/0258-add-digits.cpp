27aa8a3ffd78c7bfd5a85fcc03d3d41dbd625835
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-19 12:47:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.78%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int s=0;
-        while(num){
-           s+=num%10;
-           num/=10;
-          
-        }
-         if(s<10)return s;
-           else
-           return addDigits(s);
+        if(num == 0) return 0;
 
+    if(num %9 ==0) return 9;
+    
+    return num %9;
     }
 };
