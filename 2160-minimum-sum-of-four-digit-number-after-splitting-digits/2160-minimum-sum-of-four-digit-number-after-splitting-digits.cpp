648b217aa391a1061a4c75728bba957fb099cbb7
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-21 20:06:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (90.33%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -4,14 +4,15 @@ public:
         vector<int> temp;
 
         while (num) {
-            temp.push_back( num % 10);
+            int rem = num % 10;
+            temp.push_back(rem);
             num /= 10;
         }
 
         sort(temp.begin(), temp.end());
 
-        int x = temp[0]*10+temp[2];
-        int y = temp[1]*10+temp[3];
+        int x = temp[0] * 10 + temp[2];
+        int y = temp[1] * 10 + temp[3];
 
         return x + y;
     }
