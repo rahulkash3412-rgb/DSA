c3dd738a8a7aaef2bfbb945790d67c598d0645e4
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-23 11:08:47 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.75%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -3,10 +3,10 @@ public:
     int commonFactors(int a, int b) {
        
         int count =0;
-        for(int i=1;i<=a;i++){
-            if( a % i == 0 && b % i == 0 ){
+        for(int i=1;i<=min(a,b);i++){
+            if( a%i==0 && b%i==0 )
                 count++;
-            }
+            
         }
         return count;
     }
