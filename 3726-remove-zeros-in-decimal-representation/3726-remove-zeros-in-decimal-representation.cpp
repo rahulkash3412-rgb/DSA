5a16cac0594c0f0bd25a5a6483a98decbd738533
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-24 12:55:58 +0530

    Time: 0 ms (100%), Space: 9.2 MB (54.77%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.cpp
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     long long removeZeros(long long n) {
-       string ans;
-       string s =to_string(n);
-       for(char c : s){
-        if(c != '0')
-          ans += c;
+      long long res1=0;
+      long ans=0;
+      while(n>0){
+       int a = n%10;
+       if(a!=0) res1 = res1*10 +a;
+       n /= 10;
        }
-        return stol(ans);
+       while(res1>0){
+          ans = ans*10 +(res1%10);
+          res1 /= 10;
+       }
+       return ans;
     }
 };
