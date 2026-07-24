e0a18345dea65831faaecda2da935dff21c592ed
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-24 20:00:58 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.03%) - LeetHub

--- 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int findDelayedArrivalTime(int at, int dt) {
         int ans = at+dt;
-        if(ans>= 24){
+        
         ans = ans%24;
             return ans;
-        }
-        else return ans;
+    
     }
 };
