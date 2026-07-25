08f2ce2810e111fde28a35618f98d06cd4dc7741
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-25 13:57:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (41.25%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) { for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == ' ') {
+                k--;
+                if (k == 0) {
+                    return s.substr(0, i);
+                }
+            }
+        }
+        return s;
+    }
+};
