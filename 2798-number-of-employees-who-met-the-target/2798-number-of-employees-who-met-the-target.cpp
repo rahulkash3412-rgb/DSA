c3fad0b9f817a8e76ae49b25c6f9df24a0ad9324
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-07-25 13:19:42 +0530

    Time: 0 ms (100%), Space: 24.6 MB (51.33%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -3,9 +3,7 @@ public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
         int ans=0;
         for (int i: hours){
-            if(i>=target){
-                ans++;
-            }
+           ans += (i>=target)?1:0;
         }
 
         return ans;
