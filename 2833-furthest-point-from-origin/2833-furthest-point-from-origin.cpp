b43abfc13eb56263ec96d3a60faa5bfc64db1266
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-08-01 20:00:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (30.81%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
         int ans=0;
-        int y=0,x=0;
+        int y=0;
         for(auto ch : moves){
             if(ch=='_') ans++;
             else if(ch=='L') y++;
-            else x++;
+            else y--;
         }
-        return abs(ans+abs(x-y));
+        return (ans+abs(y));
     }
 };
