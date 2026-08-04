3dcddff600e21aa225b4489516e26a015d315596
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-08-04 13:41:07 +0530

    Time: 0 ms (100%), Space: 32.1 MB (99.38%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
         vector<int> ans ;
-        int n = nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            if(nums[i+1]-nums[i] == 1) continue ;
-            int j = nums[i] +1 ;
-            while( j != nums[i+1]){
-                ans.push_back(j);
-                j++;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int x = nums[i - 1] + 1; x < nums[i]; x++) {
+                ans.push_back(x);
             }
         }
+
+        
         return ans;
     }
 };
