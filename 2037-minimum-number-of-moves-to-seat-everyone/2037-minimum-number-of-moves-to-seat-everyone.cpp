856be1b91fc450f3c71175319ba762de37582055
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-08-04 20:33:11 +0530

    Time: 0 ms (100%), Space: 21.9 MB (30.73%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -4,8 +4,10 @@ public:
         int cnt=0;
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
-        for(int i=0;i<seats.size();i++){
+        int i=0;
+        while(i<seats.size()){
              cnt = abs(seats[i]-students[i])+cnt;
+             ++i;
         }
         return cnt;
     }
