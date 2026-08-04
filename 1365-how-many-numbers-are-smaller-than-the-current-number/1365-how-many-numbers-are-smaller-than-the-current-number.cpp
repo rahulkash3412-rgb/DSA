71af773ab670339cf81b43a0eefbe236040669ea
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-08-04 20:20:57 +0530

    Time: 6 ms (54.27%), Space: 14.3 MB (32.07%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<n.size();i++){
             int cnt=0;
             for(int j=0;j<n.size();j++){
-                 if(n[i]>n[j])cnt++;
+                 if(n[j]<n[i])cnt++;
             }
             ans.push_back(cnt);
         }
