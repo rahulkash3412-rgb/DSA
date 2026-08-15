c626716758a0dde39f86efca410fdf3df93a0f37
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-08-15 13:42:52 +0530

    Time: 25 ms (7.44%), Space: 38.8 MB (15.3%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -13,15 +13,14 @@ int sumofceil(vector<int>arr ,int div){
         sort(nums.begin(),nums.end());
         int low = 1;
         int high = nums[nums.size()-1];
-            int ans=low; 
+         
         while(low<=high){
             int mid = low+ ( high- low)/2 ;
             if(sumofceil(nums , mid) <= threshold){
-                ans = mid ;
                 high =mid-1;
             }
             else low =mid+1 ; 
         }
-        return ans;
+        return low;
     }
 };
