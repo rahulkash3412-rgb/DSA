9b3c46167a79ec63bb4503ebf52bf9e24ccaac94
rahulkash3412-rgb <rahulkash3412@gmail.com>
2026-08-17 21:18:54 +0530

    Time: 0 ms (100%), Space: 10.6 MB (43.54%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+int fxn(vector<int>& a ,long long sum){
+   int ele=1;
+   long long cursum=0;
+   for(int it : a){
+    if(cursum + it <= sum)cursum += it ;
+    else {
+    ele++;
+    cursum=it;
+         }
+                 }
+   
+return ele;
+}
+
+
+public:
+    int splitArray(vector<int>& a, int k) {
+        int low = *max_element(a.begin(),a.end());
+long long high = accumulate(a.begin(),a.end(),0LL);
+while(low<=high){
+    long long mid = (low+high)/2;
+   int elements=fxn(a,mid);
+    if(elements>k) low = mid+1;
+    else {high = mid-1;}
+    }
+    return low;
+}
+};
